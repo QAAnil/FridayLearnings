890ba00ac2fef1c9a9c3279e1dece1172e1651a9
AnilP <anil.putta@creditsafe.com>
2024-06-06 13:49:34 +0530

    FirstTest

---
+++ Learn JavaScript/ternaryoperator.js
@@ -0,0 +1,7 @@
+let age = 18;
+let output = age >= 18 ? "Adult" : "Minor";
+console.log(output); // Output: Adult
+
+let score = 85;
+let grade = score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : "D";
+console.log(grade); // Output: B
